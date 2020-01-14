f1ee94ffe3780af020e925b2d45648fa2aa6721a
ambaranow <ambaranow@gmail.com>
2020-01-14 11:50:47 +0300

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatProgressBarModule, MatIconModule, MatCardModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiTaskComponent } from './background-tasks/components/ui-task/ui-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
